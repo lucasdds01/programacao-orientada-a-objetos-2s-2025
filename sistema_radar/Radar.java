71fde4297438e70ca270e8ed839a4e761411c9f1
lucasdds01 <lucas.drdmg.silva@gmail.com>
2025-09-01 09:49:52 -0300

    add carro, radar e simulação

---
+++ sistema_radar/Radar.java
@@ -0,0 +1,21 @@
+package sistema_radar;
+
+public class Radar {
+    public String localizacao;
+    public Integer limiteVelocidade;
+
+    public void emitirNoitificacao(String placa, Integer velocidadeObservada)
+    {
+        System.out.println("Placa: "+placa);
+        System.out.println("Velocidade observada: "+velocidadeObservada);
+        System.out.println("Localização: "+this.localizacao);
+        System.out.println("Velocidade da via: "+this.limiteVelocidade);
+    }
+    public void avaliarVelocidade(Carro carro)
+    {
+        if(carro.velocidade > this.limiteVelocidade)
+        {
+            emitirNoitificacao(carro.placa, carro.velocidade);
+        }
+    }
+}
---
+++ sistema_radar/Simulacao.java
@@ -0,0 +1,25 @@
+package sistema_radar;
+
+public class Simulacao {
+    public static void main(String[] args) {
+        Carro supra=new Carro();
+        supra.ano = 94;
+        supra.modelo = "MKIV";
+        supra.placa ="LDLW589";
+        supra.velocidade = 0;
+
+        Radar radar=new Radar();
+        radar.localizacao = "Pistão Sul";
+        radar.limiteVelocidade = 60;
+        radar.avaliarVelocidade(supra);
+
+        supra.acelerar();
+        supra.acelerar();
+        supra.acelerar();
+        supra.acelerar();
+        supra.acelerar();
+        supra.acelerar();
+        supra.acelerar();
+        radar.avaliarVelocidade(supra);
+    }
+}
